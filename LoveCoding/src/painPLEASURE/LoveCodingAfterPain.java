269e7efd224e9e4b62a45b6e0af279c44e8107b9
jmintegr1 <jmintegr1@gmail.com>
2018-12-18 22:01:13 -0500

    Now I love codig because I am bulldog!

---
+++ LoveCoding/src/painPLEASURE/LoveCodingAfterPain.java
@@ -0,0 +1,9 @@
+package painPLEASURE;
+
+public class LoveCodingAfterPain {
+
+	public static void main(String[] args) {
+		System.out.println("I learn to love coding and pushing past pain");
+	}
+
+}
